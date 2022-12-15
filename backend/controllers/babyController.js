ca400ee135201c73db8f840eef99abb8f0459608
CodyConck <codyconck@gmail.com>
2022-12-15 16:06:48 -0600

    restAPI for babies

--- backend/controllers/babyController.js
+++ backend/controllers/babyController.js
@@ -1,10 +1,14 @@
 const asyncHandler = require("express-async-handler");
 
+const Baby = require("../models/babyModel");
+
 // @desc      Get babies
 // @route     GET /api/babies
 // @access    Private
 const getBabies = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get babies" });
+  const babies = await Baby.find();
+
+  res.status(200).json(babies);
 });
 
 // @desc      Set baby
@@ -18,24 +22,48 @@ const setBaby = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field");
   }
 
+  const baby = await Baby.create({
+    text: req.body.text,
+  });
+
   //   // console logs body data from put route sent in postman
   //   console.log(req.body);
 
-  res.status(200).json({ message: "set/create baby" });
+  res.status(200).json(baby);
 });
 
 // @desc      Update babies
 // @route     PUT /api/babies/:id
 // @access    Private
 const updateBaby = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update baby ${req.params.id}` });
+  const baby = await Baby.findById(req.params.id);
+  // if theres no baby, respond with 400 status
+  if (!baby) {
+    res.status(400);
+    throw new Error("Baby not found");
+  }
+
+  const updatedBaby = await Baby.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedBaby);
 });
 
 // @desc      Delete baby
 // @route     DELETE /api/babies/:id
 // @access    Private
 const deleteBaby = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete baby ${req.params.id}` });
+  const baby = await Baby.findById(req.params.id);
+  // if theres no baby, respond with 400 status
+  if (!baby) {
+    res.status(400);
+    throw new Error("Baby not found");
+  }
+
+  await baby.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
