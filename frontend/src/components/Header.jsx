6dd02bd3f392721f604eae4bb8f048aa4042dc76
CodyConck <codyconck@gmail.com>
2022-12-28 11:50:14 -0600

    updated header before checking out for another feature

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">BabySetter</Link>
+        <Link to="/">BabyTracker</Link>
       </div>
       <ul>
         <li>
