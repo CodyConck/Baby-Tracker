e8c2b52de6ab0f3bdb16a5779389d156ca2b6c0d
CodyConck <codyconck@gmail.com>
2022-12-28 12:27:08 -0600

    added register/login forms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,74 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
+// state
 function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // destructure above field
+  const { email, password } = formData;
+
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      // this targets the name field on the form and uses its value
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  //jsx
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and get started</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              // this comes from above formData
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              // this comes from above formData
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Login;
