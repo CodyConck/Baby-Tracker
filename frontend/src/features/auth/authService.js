a418d05386a15c694763013b8a26ec020659bb10
CodyConck <codyconck@gmail.com>
2023-01-03 15:53:39 -0600

    completed logout

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -15,7 +15,13 @@ const register = async (userData) => {
   return response.data;
 };
 
+// logout user
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
 const authService = {
   register,
+  logout,
 };
 export default authService;
